af448bbd8e94dc53094a502528c22f68fffd4900
Toxin4426 <Toxin4426@gmail.com>
2025-10-11 12:06:25 -0400

    Pause menu

--- JamFestGame/Assets/Scripts/UI/PauseMenuController.cs
+++ JamFestGame/Assets/Scripts/UI/PauseMenuController.cs
@@ -3,12 +3,32 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenuController : MonoBehaviour
 {
- 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(transform.GetChild(0).gameObject.activeSelf)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f; 
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
 
     public void ResumeGame()
     {
+        print(1);
         Time.timeScale = 1f;
-        gameObject.SetActive(false); 
+        transform.GetChild(0).gameObject.SetActive(false);
     }
 
     public void OpenSettings()
